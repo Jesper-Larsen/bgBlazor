2e702d6eb61ef2afb2464d2f9fb67116152d73de
JonesTheG <4aflarsen@familie.tele.dk>
2022-03-29 14:03:29 +0200

    Add project files.

---
+++ bgBlazor/bgBlazor/Client/Pages/Index.razor.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using bgBlazor.Client;
+using bgBlazor.Client.Shared;
+
+namespace bgBlazor.Client.Pages
+{
+    public partial class Index
+    {
+
+        private RenderFragment? DynamicContent { get; set; }
+
+        public int SimpleParam1 { get; set; }
+        public int AdvancedParam1 { get; set; }
+
+        protected override void OnInitialized()
+        {
+
+            SimpleParam1 = 0;
+            AdvancedParam1 = 2;
+
+            ShowSimpleCoding();
+            base.OnInitialized();
+        }
+
+        private void ShowSimpleCoding()
+        {
+            DynamicContent = builder =>
+            {
+                builder.OpenComponent(0, typeof(SimpleCoding));
+                builder.AddAttribute(1, "PageParam1", SimpleParam1);
+                builder.CloseComponent();
+            };
+        }
+        private void ShowLongSimple()
+        {
+            SimpleParam1 = 1;
+        }
+
+        private void ShowAdvancedCoding()
+        {
+            DynamicContent = builder =>
+            {
+                builder.OpenComponent(0, typeof(AdvancedCoding));
+                builder.AddAttribute(1, "PageParam1", AdvancedParam1);
+                builder.CloseComponent();
+            };
+        }
+
+        private void ShowLongAdvanced()
+        {
+            AdvancedParam1 = 3;
+        }
+
+    }
+}
